f14610111183677fa86aa62ebdd9d89b7db7e8ff
Vadim <vadim.mozeiko@gmail.com>
2021-02-10 09:41:47 +0200

    initial commit

---
+++ components/formRender.js
@@ -0,0 +1,16 @@
+function formRender(selector) {
+        // input validation
+
+
+
+
+        //logic
+
+    const DOM = document.querySelector(selector)
+
+    let HTML  = ''
+
+    for (let i = 0; i < 4; i++){
+        HTML += ``
+    }
+}
